d0ab5b7411219720510c524e1f61cf099d89ec11
Denis Putnam <denisputnam@verizon.net>
2017-04-17 16:33:55 -0400

    Made Car.java serializable.

--- PrimeFacesExample/src/main/java/com/denis/primefaces/domain/Car.java
+++ PrimeFacesExample/src/main/java/com/denis/primefaces/domain/Car.java
@@ -1,19 +1,25 @@
 package com.denis.primefaces.domain;
 
-public class Car {
+import java.io.Serializable;
+
+public class Car implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1974245269446217290L;
 	private String id;
 	private String brand;
 	private Integer year;
 	private String color;
-	private Integer mileage;
+	private Integer price;
 	private Boolean sold;
 	
-	public Car( String vin, String brand, Integer year, String color, Integer mileage, Boolean sold){
+	public Car( String vin, String brand, Integer year, String color, Integer price, Boolean sold){
 		this.id = vin;
 		this.brand = brand;
 		this.year = year;
 		this.color = color;
-		this.mileage = mileage;
+		this.price = price;
 		this.sold = sold;
 	}
 	public String getId() {
@@ -40,11 +46,11 @@ public class Car {
 	public void setColor(String color) {
 		this.color = color;
 	}
-	public Integer getMileage() {
-		return mileage;
+	public Integer getPrice() {
+		return price;
 	}
-	public void setMileage(Integer mileage) {
-		this.mileage = mileage;
+	public void setPrice(Integer price) {
+		this.price = price;
 	}
 	public Boolean getSold() {
 		return sold;
@@ -52,6 +58,26 @@ public class Car {
 	public void setSold(Boolean sold) {
 		this.sold = sold;
 	}
-	
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Car other = (Car) obj;
+        if ((this.id == null) ? (other.id != null) : !this.id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
 
 }
